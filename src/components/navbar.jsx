9a7feba4196b8d0689f20a246e51bb3f8b899321
Manjula Gotur <manjulagotur2@gmail.com>
2024-02-16 23:29:06 +0530

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../styles/navbar.css"
+
+const Navbar = () => {
+
+  return (
+    <div className='navbar'>
+      <img src="" alt=""  className='logo'/>
+      <h1>TINY OCEAN</h1>
+  <ul>
+    <li>Home</li>
+    <li>Shop</li>
+    <li>About</li>
+    <li>Customer Care</li>
+    <li>Login</li>
+    </ul>  
+
+    <div className="search-box">
+
+    </div>
+    </div>
+    
+  )
+}
+
+export default Navbar
